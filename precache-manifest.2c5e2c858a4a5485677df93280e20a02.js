d6147ff1e3fa0199d32e680aecbffeffe9fb7793
bsystem64 <brunobastos_1998@hotmail.com>
2020-04-18 17:54:14 -0300

    page demo

---
+++ precache-manifest.2c5e2c858a4a5485677df93280e20a02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab925d94368d858287fa93164a60449",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5afe47bd713b620a65c1",
+    "url": "/static/css/main.9240b197.chunk.css"
+  },
+  {
+    "revision": "8b9b3423f2f18dd4b105",
+    "url": "/static/js/2.0cefe607.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0cefe607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5afe47bd713b620a65c1",
+    "url": "/static/js/main.69ea48ea.chunk.js"
+  },
+  {
+    "revision": "9241cd3a90ce835616b6",
+    "url": "/static/js/runtime-main.98269123.js"
+  }
+]);
